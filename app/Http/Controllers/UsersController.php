2e9d71bd8057463901b663199b0b1e854c95e0d8
huangmeidneg <huangmeidneg@outlook.com>
2018-01-15 09:26:50 +0000

    个人信息编辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -16,5 +17,22 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    /**
+     * @author: Kinano
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * 编辑资料
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
